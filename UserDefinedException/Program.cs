88e0d52de21403e3b51660d8e3b3e8a01f86977e
riya-gupta-ns <97439780+riya-gupta-ns@users.noreply.github.com>
2022-02-09 18:57:19 +0530

    Add files via upload

---
+++ UserDefinedException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+/// <summary>
+/// user defined class
+/// </summary>
+class UserDefineException: Exception
+{
+
+    public UserDefineException(string str)
+    {
+        Console.WriteLine("In user Defined Class");
+       // Console.WriteLine(str);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        String writeText="User Define Exception";
+        UserDefineException M = new UserDefineException(writeText);
+       //UserDefineException M = new UserDefineException("User Defined Exception");
+
+        try
+        {
+         throw M;
+           //File.WriteAllText("filename1.txt",writeText);
+
+           //string readText=File.ReadAllText("filename1.txt");
+           //Console.WriteLine(readText);
+        }
+        catch (Exception e)
+        {
+            File.WriteAllText("filename1.txt",e.Message);
+
+            string readText=File.ReadAllText("filename1.txt");
+            Console.WriteLine(readText);
+           // Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finally Block");
+        }
+    }
+}
